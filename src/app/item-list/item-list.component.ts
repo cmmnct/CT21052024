6753903568ea9445adaf74cd6d0d91021e8c5c8b
CMMNCT <mark@cmmnct.nl>
2024-06-05 11:40:59 +0200

    Added item-list with filter based op service with RxJS (Declarative)

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FilterService } from '../services/filter.service';
 import { Observable } from 'rxjs';
-import { FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
-  standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor, AsyncPipe],
   selector: 'app-item-list',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgFor, NgIf, AsyncPipe],
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent implements OnInit {
   items$!: Observable<string[]>;
   filterControl: FormControl = new FormControl('');
-
-  
 
   constructor(private filterService: FilterService) {}
 
